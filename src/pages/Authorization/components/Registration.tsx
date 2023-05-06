8db8dd7297dcd609414ee15007e2420ee1fd03ac
damir <qw>
2023-05-06 21:01:50 +0300

    add: to Registration.tsx initial inputs

--- src/pages/Authorization/components/Registration.tsx
+++ src/pages/Authorization/components/Registration.tsx
@@ -8,8 +8,7 @@ const Registration = () => {
   );
   // React.ChangeEvent<HTMLSelectElement>
   const sortByGovernanceChange = (index: number) => {
-    const governanceIndex = index;
-    setDepartments(AuthConst.departments[governanceIndex].departments);
+    setDepartments(AuthConst.departments[index].departments);
   };
 
   return (
@@ -17,9 +16,38 @@ const Registration = () => {
       <h1 className="text-orange font-[200] tracking-widest text-3xl">
         Регистрация
       </h1>
-      <UI.Custom.RowInput type="text" placeholder="Введите логин" />
-      <UI.Custom.RowInput type="text" placeholder="Введите пароль" />
-      <UI.Custom.RowInput type="password" placeholder="Повторите пароль" />
+      <UI.Custom.RowInput
+        type="text"
+        placeholder="Введите логин"
+        required={true}
+      />
+      <div className="flex flex-row justify-between w-full gap-2">
+        <UI.Custom.RowInput
+          type="text"
+          placeholder="Введите фамилию"
+          required={true}
+        />
+        <UI.Custom.RowInput
+          type="text"
+          placeholder="Введите имя"
+          required={true}
+        />
+      </div>
+      <UI.Custom.RowInput
+        type="text"
+        placeholder="Введите отчество"
+        required={true}
+      />
+      <UI.Custom.RowInput
+        type="text"
+        placeholder="Введите пароль"
+        required={true}
+      />
+      <UI.Custom.RowInput
+        type="password"
+        placeholder="Повторите пароль"
+        required={true}
+      />
       <div className="flex flex-row justify-between w-full gap-2">
         <UI.Custom.Select
           options={AuthConst.governance.map((el) => el.title)}
